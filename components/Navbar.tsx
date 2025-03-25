2b8c5ee50b336a7a76069deb6bd47186cd5495b2
lesha358 <lesha358@github.com>
2025-03-25 15:07:47 +0300

    Исправлено дублирование навигационного меню: обновлены стили и структура компонентов

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -27,7 +27,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className={`fixed w-full z-50 transition-all duration-300 ${
+    <nav className={`w-full z-50 transition-all duration-300 ${
       isScrolled ? 'bg-white shadow-md' : 'bg-transparent'
     }`}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,14 @@ import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Navbar />
+    <div className="min-h-screen bg-gray-50">
+      <div className="fixed top-0 left-0 right-0 z-50">
+        <Navbar />
+      </div>
       <div className="pt-16">
         <Component {...pageProps} />
       </div>
-    </>
+    </div>
   );
 }
 
